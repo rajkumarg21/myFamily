7e12162d353645187e3b77120f14bb2301d4a8e0
Rajkumar saad <58967581+rajkumarg21@users.noreply.github.com>
2024-05-01 18:14:12 +0530

    Add files via upload
    Initial-Commit

---
+++ myFamily/src/main/java/com/myFamily/MyFamilyApplication.java
@@ -0,0 +1,13 @@
+package com.myFamily;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFamilyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFamilyApplication.class, args);
+	}
+
+}
